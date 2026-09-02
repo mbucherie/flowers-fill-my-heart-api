464a5c82ce0782f8f4541b2f67dbbc8e35d34d1e
mbucherie <malibucherie@outlook.com>
2026-09-02 13:16:32 -0700

    Add database environment diagnostics

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,12 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_PASSWORD exists:", !!process.env.DB_PASSWORD);
+console.log("DB_PASSWORD length:", process.env.DB_PASSWORD ? process.env.DB_PASSWORD.length : 0);
+
+
 const pool = mysql.createPool({
   socketPath: "/var/lib/mysql/mysql.sock",
   user: process.env.DB_USER,
